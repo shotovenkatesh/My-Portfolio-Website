cca966470422be313e7c4febeb30fb3f2adc29fa
Venkatesh <shotovenkatesh705@gmail.com>
2021-10-08 15:57:51 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import smtplib
+
+app = Flask(__name__)
+
+FROM_EMAIL = os.environ.get("FROM_EMAIL")
+TO_EMAIL = os.environ.get("TO_EMAIL")
+PASS = os.environ.get("PASSWORD")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form["name"]
+        mail = request.form["email"]
+        message = request.form["message"]
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=FROM_EMAIL,
+                             password=PASS)
+            connection.sendmail(from_addr=FROM_EMAIL,
+                                to_addrs=TO_EMAIL,
+                                msg=f"Subject:A message from your portfolio website \n\n. Hi my name is{name}.{message}. This is my email id: {mail}")
+        return render_template("index.html",sent = True)
+    return render_template("index.html",sent = False)
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
